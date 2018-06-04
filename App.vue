be2c1023d82b0ba82888e7f012d88460033c09a7
Arnelle Balane <arnellebalane@gmail.com>
2018-06-04 09:48:51 +0800

    Add AddToCartButton component

--- App.vue
+++ App.vue
@@ -1,17 +1,33 @@
 <template>
     <div class="app" id="app">
         <CartWidget />
+        <AddToCartButton />
     </div>
 </template>
 
 <script>
+    import AddToCartButton from './components/AddToCartButton.vue';
     import CartWidget from './components/CartWidget.vue';
 
     export default {
         name: 'App',
 
         components: {
+            AddToCartButton,
             CartWidget
         }
     };
 </script>
+
+<style scoped>
+    .app {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .add-to-cart-button {
+        margin-top: 50px;
+    }
+</style>
---
+++ components/AddToCartButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button
+        class="add-to-cart-button"
+        v-bind="$attrs"
+        v-on="$listeners"
+    >
+        Add to Cart
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'AddToCartButton'
+    };
+</script>
+
+<style scoped>
+    .add-to-cart-button {
+        display: inline-block;
+        padding: 1.5em 3em;
+        border: none;
+        font: inherit;
+        font-size: 18px;
+        text-transform: uppercase;
+        color: #fff;
+        background-color: #ff3355;
+        cursor: pointer;
+        transition: opacity 300ms ease;
+    }
+
+    .add-to-cart-button:hover,
+    .add-to-cart-button:focus {
+        opacity: 0.5;
+    }
+</style>
