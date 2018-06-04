e25f916d67634e71a8b67ab4342cdd047cbf7d56
Arnelle Balane <arnellebalane@gmail.com>
2018-06-04 08:49:59 +0800

    Initial project setup

---
+++ App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="app" id="app">
+        <h1>Add-to-Cart Animation</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    };
+</script>
